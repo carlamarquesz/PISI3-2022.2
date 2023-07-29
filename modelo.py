f0654a528b2adfeaca1706600433756e25d2df71
João Manoel <125512817+joaomanoel1532@users.noreply.github.com>
2023-07-29 20:27:57 -0300

    Add files via upload
    pagina de previsão de crédito com o modelo de árvore de decisão

---
+++ modelo.py
@@ -0,0 +1,54 @@
+from utils import *  
+from sklearn.model_selection import StratifiedShuffleSplit 
+from sklearn.metrics import accuracy_score,confusion_matrix,precision_score,recall_score
+from sklearn import tree 
+import matplotlib.pyplot as plt 
+
+
+
+def executar_validador(X, y):
+    validador = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=123)
+    for treino_id, teste_id in validador.split(X, y):
+        X_train, X_test = X[treino_id], X[teste_id]
+        y_train, y_test = y[treino_id], y[teste_id]
+    return X_train, X_test, y_train, y_test
+
+
+def salvar_arvore(classificador, nome):
+    plt.figure(figsize=(200, 100))
+    tree.plot_tree(classificador, filled=True, fontsize=14)
+    plt.savefig(nome)
+    plt.close()
+
+
+def executar_classificador(classificador, X_train, X_test, y_train):
+    arvore = classificador.fit(X_train, y_train)
+    y_pred = arvore.predict(X_test)
+    return y_pred
+
+def validar_arvore(y_test, y_pred):
+    acuracia = accuracy_score(y_test, y_pred)
+    precisao = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    matrix_confusao = confusion_matrix(y_test, y_pred)
+    return acuracia, precisao, recall, matrix_confusao 
+
+# execucao do validador
+X = dados.drop("TARGET", axis=1).values
+y = dados["TARGET"].values
+X_train, X_test, y_train, y_test = executar_validador(X, y)
+
+# execucao do classificador DecisionTreeClassifier
+classificador_arvore_decisao = tree.DecisionTreeClassifier(
+    max_depth=10, random_state=123
+)
+y_pred_arvore_decisao = executar_classificador(
+    classificador_arvore_decisao, X_train, X_test, y_train
+) 
+
+# validacao arvore de decisao
+acuracia,precisao,recall,matrix_confusao = validar_arvore(y_test, y_pred_arvore_decisao)
+print(dados.columns)  
+
+# criacao da figura da arvore de decisao
+#salvar_arvore(classificador_arvore_decisao, "arvore_decisao.png")
