5fadfecd26977e16f86ff7f425034919df59121f
Lucas de Morais <lucas.morais1318@gmail.com>
2023-07-28 22:09:44 -0300

    Tratamento de variáveis categóricas e média de pgt

--- utils.py
+++ utils.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
-# from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE
 
 # Reorganizando os dados credit_card_approval para ML
 # df = pd.read_csv("./data/credit_card_approval.csv")
 df = pd.read_parquet("./data/credit_card_approval.parquet")
+df = df.sort_values(by='ID')
 
 col_em_ing  = colunas_db = list(df.columns)
 
@@ -36,72 +37,35 @@ df.columns = new_columns
 df["GENERO"].replace({"F": 0, "M": 1}, inplace=True)
 df["POSSUI_CARRO"].replace({"Y": 1, "N": 0}, inplace=True)
 df["POSSUI_PROPRIEDADES"].replace({"Y": 1, "N": 0}, inplace=True)
-df["QTD_FILHOS"].replace(
-    {"No children": 0, "1 children": 1, "2+ children": 2}, inplace=True
-)
-df["ESCOLARIDADE"].replace(
-    {
-        "Higher education": 1,
-        "Secondary / secondary special": 2,
-        "Incomplete higher": 3,
-        "Lower secondary": 4,
-        "Academic degree": 5,
-    },
-    inplace=True,
-)
-df["ESTADO_CIVIL"].replace(
-    {
-        "Civil marriage": 1,
-        "Married": 2,
-        "Single / not married": 3,
-        "Separated": 4,
-        "Widow": 5,
-    },
-    inplace=True,
-)
-df["TIPO_DE_MORADIA"].replace(
-    {
-        "Rented apartment": 1,
-        "House / apartment": 2,
-        "Municipal apartment": 3,
-        "With parents": 4,
-        "Co-op apartment": 5,
-        "Office apartment": 6,
-    },
-    inplace=True,
-)
 df["DIAS_ANIVERSARIO"] = np.ceil(
     pd.to_timedelta(df["DIAS_ANIVERSARIO"], unit="D").dt.days * (-1)
 )
 df["POSSUI_EMPREGO"] = np.where(df["POSSUI_EMPREGO"] < 0, 1, 0)
-df["CARGO"].replace(
-    {
-        "Security staff": 1,
-        "Sales staff": 2,
-        "Accountants": 3,
-        "Laborers": 4,
-        "Managers": 5,
-        "Drivers": 6,
-        "Core staff": 7,
-        "High skill tech staff": 8,
-        "Cleaning staff": 9,
-        "Private service staff": 10,
-        "Cooking staff": 11,
-        "Low-skill Laborers": 12,
-        "Medicine staff": 13,
-        "Secretaries": 14,
-        "Waiters/barmen staff": 15,
-        "HR staff": 16,
-        "Realty agents": 17,
-        "IT staff": 18,
-    },
-    inplace=True,
-)
 df["QTD_MESES"] = np.ceil(pd.to_timedelta(df["QTD_MESES"], unit="D").dt.days * (-1))
 df["STATUS_PAGAMENTO"].replace(
-    {"C": 1, "X": 1, "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0},
+    {"C": 1, "X": 0, "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0},
     inplace=True,
 )
+
+
+
+#Técnica One-Hot
+tipos_de_profissao = pd.get_dummies(df["CARGO"])
+tipos_estado_civil = pd.get_dummies(df["ESTADO_CIVIL"])
+tipos_de_moradia = pd.get_dummies(df["TIPO_DE_MORADIA"])
+tipos_escolaridade = pd.get_dummies(df["ESCOLARIDADE"])
+qtda_filhos = pd.get_dummies(df["QTD_FILHOS"])
+df = pd.concat([df, tipos_de_profissao, tipos_estado_civil, tipos_de_moradia, tipos_escolaridade, qtda_filhos], axis= 1)
+
+#AVALIANDO CLIENTE
+media_pagamentos = df.groupby('ID')['STATUS_PAGAMENTO'].mean().reset_index()
+media_pagamentos.rename(columns={'STATUS_PAGAMENTO': 'MEDIA_PAGAMENTO'}, inplace=True)
+df = df.merge(media_pagamentos, on='ID', suffixes=('', '_Media'))
+df['BOM_CLIENTE'] = df['MEDIA_PAGAMENTO'].apply(lambda media: 1 if media >= 0.6 else 0)
+
+
+del df["CARGO"], df['ESCOLARIDADE'], df['ESTADO_CIVIL'], df['QTD_FILHOS'], df['TIPO_DE_MORADIA']
+
 
 def texto(filtro_qtd_meses):
     if filtro_qtd_meses == 0:
@@ -131,9 +95,9 @@ ax2.set_title('Target balanceado com SMOTE')
 #plt.show()
 print(df['TARGET'].value_counts())
 print(df.head(2))
-df = df.drop(columns=['ID', 'GENERO', 'DIAS_ANIVERSARIO', 'CELULAR', 'TELEFONE_COMERCIAL', 'TELEFONE_RESIDENCIAL', 'EMAIL', 'QTD_MESES'])
+df = df.drop(columns=['ID', 'GENERO', 'DIAS_ANIVERSARIO', 'CELULAR', 'TELEFONE_COMERCIAL', 'TELEFONE_RESIDENCIAL', 'EMAIL', 'QTD_MESES', 'STATUS_PAGAMENTO'])
 print(df.head(2))
-Xmaria = [[1,1,2,42000,1,4,2,1,8,1]]
+Xmaria = [[1, 1, 0, 1, 40000, 10600, 1, 1, 1, 1, 1, ]]
 #Divisão em inputs e outputs
 X = df.drop('TARGET', axis = 1)
 y = df['TARGET']
