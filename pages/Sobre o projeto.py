815263f6e29bdb28c6d7135f807373d3461e0c66
Antony Albuquerque <Antony.albuquerque@ufrpe.br>
2023-08-01 00:33:59 -0300

    criação da page sobre o projeto

---
+++ pages/Sobre o projeto.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('ST Credit :coin:')
+
+st.subheader('Objetivo')
+st.markdown("""<div style="text-align: justify;"> 
+                    O objetivo geral do nosso projeto é realizar uma análise de dados de clientes bancários que desejam verificar a elegibilidade de obter um cartão de crédito. Com base nos dados disponíveis, aplicando técnicas de machine learning que permitirá uma compreensão aprofundada dos clientes, auxiliando no desenvolvimento de estratégias personalizadas, detecção de riscos e otimização dos processos de tomada de decisão.""",unsafe_allow_html= True)
+st.subheader(' ')
+st.subheader('Objetivos especificos')
+st.markdown('- Prever aprovação de crédito;')
+st.markdown('- Realizar uma análise exploratória dos dados, identificando características importantes e padrões de comportamento dos clientes;')
+st.markdown('- Identificar segmentações de clientes com base nos dados disponíveis, permitindo o desenvolvimento de estratégias personalizadas.')
+
